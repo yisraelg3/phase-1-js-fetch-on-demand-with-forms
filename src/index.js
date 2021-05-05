b5468f3f345389724259018ecbae7dc6a23b58dc
Yisrael Gurkow <yisrelg3@gmail.com>
2021-05-05 14:52:39 -0400

    commit tree b3fe63bb150f38bb67d91de5815c715a1cab35c9

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
+
 const init = () => {
-  
+    const inputForm = document.querySelector('form')
+    const input = document.querySelector('input#searchByID');
+    const title = document.querySelector('#movieDetails h4')
+    const details = document.querySelector('#movieDetails p')
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+        //console.log(event.target.children[1].value)
+        //console.log(input.value)
+
+        fetch(`http://localhost:3000/movies/${event.target.searchByID.value}`)
+        .then(res => res.json())
+        //.then(console.log)
+        .then((movie) => {
+            title.innerText = movie.title
+            details.innerText = movie.summary})
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
